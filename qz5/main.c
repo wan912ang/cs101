8e8f43bb9f26c57cef84b0dfccf0e79a19dd4b84
wan912ang <100832097+wan912ang@users.noreply.github.com>
2022-05-19 16:28:17 +0800

    Update main.c

--- qz5/main.c
+++ qz5/main.c
@@ -1,65 +1,50 @@
 #include <stdio.h>
-#include<stdlib.h>
 
-typedef struct node {
-    int data;
-    struct node* next;
-} node_t;
-
-node_t* allocate_node(int data) {
-    node_t *newNode = malloc(sizeof(node_t));
-    newNode->data = data;
-    newNode->next = NULL;
-    return newNode;
+int do_before_adding(int *i) {
+    int before = *i;
+    *i += 1;
+    return before;
 }
 
-void show_list(node_t* list) {
-    node_t* tmp = list;
-    while(tmp != NULL) {
-        printf("[%d]->", tmp->data);
-        tmp = tmp->next;
-    }
-    printf("NULL\n");
+int do_after_adding(int *i) {
+    *i += 1;
+    int after = *i;
+    return after;
 }
 
-node_t* append_node(node_t* list, int new_data) {
-    node_t* newNode = allocate_node(new_data);
-    newNode->data = new_data;
-    newNode->next     = NULL;
-    if(list == NULL)
-         list = newNode;
-    else
-    {
-        node_t* lastNode = list;
-        while(lastNode->next != NULL)
-        {
-            lastNode = lastNode->next;
-        }
-        lastNode->next = newNode;
-    }
-    return list;
+void check_result(int x, int z, int t_x, int t_z) {
+    if(x == t_x && z == t_z) 
+        printf(" pass\n");
+    else 
+        printf(" fail\n");
 }
 
-void free_all_node(node_t* list) {
-    while (list != NULL){ 
-        node_t* temp = list; 
-        list = list -> next;
-        printf("free([%d])->",temp->data);
-        free(temp);
-    }
-    printf("NULL");
+void test_do_before_adding() {
+    int x = 1;
+    int z = 5 + do_before_adding(&x);
+    int t_x = 1;
+    int t_z = 5 + t_x++;
+    printf("do_before_adding()\n");
+    printf(" z=%d, x=%d, ", z, x);
+    printf(" t_z=%d, t_x=%d\n", t_z, t_x);
+    check_result(x, z, t_x, t_z);
+}
+
+void test_do_after_adding() {
+    int x = 1;
+    int z = 5 + do_after_adding(&x);
+    int t_x = 1;
+    int t_z = 5 + ++t_x;
+    printf("do_after_adding()\n");
+    printf(" z=%d, x=%d, ", z, x);
+    printf(" t_z=%d, t_x=%d\n", t_z, t_x);
+    check_result(x, z, t_x, t_z);
 }
 
 int main() {
-    node_t* head = NULL;
-    head = append_node(head, 0);
-    show_list(head);
-    head = append_node(head, 11);
-    show_list(head);
-    head = append_node(head, 222);
-    show_list(head);
-    head = append_node(head, 3333);
-    show_list(head);
-    free_all_node(head);
+    printf(" --- fun 1. --- ");
+    test_do_before_adding();
+    printf(" --- fun 2. --- ");
+    test_do_after_adding();
     return 0;
 }
